4cab40882ffeab3f9d11d8b41ca79bb6c36beb4a
Tinson Lai <laitinson@gmail.com>
2019-10-09 20:31:55 +1100

    Minor update

--- project.sh
+++ project.sh
@@ -1,6 +1,5 @@
 export OS_AUTH_URL=https://keystone.rc.nectar.org.au:5000/v3/
 export OS_PROJECT_ID=2adafa4646234b16b5b30646db5f4980
 export OS_PROJECT_NAME="COS-Unimelb-CIS"
-export OS_USER_DOMAIN_NAME="Default"
 export OS_PROJECT_DOMAIN_ID="default"
 export OS_REGION_NAME="Melbourne"
