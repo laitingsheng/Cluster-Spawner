7a6451fa737e61231461893cd07495e68e035e4b
Tinson Lai <laitinson@gmail.com>
2019-10-26 17:47:15 +1100

    Fix name

--- prepare.sh
+++ prepare.sh
@@ -6,8 +6,8 @@ set +eux
 kubectl label node --overwrite t-r-k8s-master node-role.kubernetes.io/master=master
 kubectl label node --overwrite t-r-k8s-minion-extra node-role.kubernetes.io/extra=extra
 # label and taint workers
-kubectl label node --overwrite t-r-k8s-minion-worker-{1..8} node-role.kubernetes.io/worker=worker
-kubectl taint nodes t-r-k8s-minion-worker-{1..8} only=workload:NoSchedule
+kubectl label node --overwrite t-r-k8s-minion-worker-{0..7} node-role.kubernetes.io/worker=worker
+kubectl taint nodes t-r-k8s-minion-worker-{0..7} only=workload:NoSchedule
 
 # create secrets
 kubectl -n kube-system create secret docker-registry custom-scheduler-registry-secret --docker-server=$K8S_SCHEDULER_REGISTRY --docker-username=$K8S_SCHEDULER_REGISTRY_USERNAME --docker-password=$K8S_SCHEDULER_REGISTRY_PASSWORD
